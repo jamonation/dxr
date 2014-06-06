d84ba50debb5dc7fba62304b7a00cb06aa05aa67
Jamon Camisso <jamon@jamon.ca>
2014-06-06 14:06:19 -0400

    Tidying annotation icons css and javascript to make them show up on lines with multiple annotations

--- dxr/static/js/annotation-icons.js
+++ dxr/static/js/annotation-icons.js
@@ -3,19 +3,22 @@
 
 $(function() {
     'use strict';
-    $('.annotation-set').hover(
+    $('.note').hover(
         function() {
             // on mouse over
             var self = $(this);
-            $('.annotation-set').css('z-index', 0);
-            self.css('z-index', 5);
+            // make the container for all annotation icons on a line visible
+            self.parent().css('z-index', 5);
+            self.parent().find('.aset-title').css('visibility', 'hidden');
+            self.next().css('visibility', 'visible');
         },
         function() {
             // on mouse out
             var self = $(this);
             //if div is not clicked, hide it, otherwise leave as is
-            if (self.hasClass('visible') === false) {
-                $(this).css('z-index', 0);
+            if (self.parent().hasClass('visible') === false) {
+                self.parent().css('z-index', 0);
+                self.next().css('visibility', 'hidden');
             }
         }
     );
@@ -23,20 +26,25 @@ $(function() {
     $('.note').click(function() {
         var self = $(this);
         //clear all other annotations first
-        $('.aset-title').removeClass('visible');
-        //make self visible
-        self.parent().addClass('visible');
+        self.parent().find('.aset-title').removeClass('visible');
+	    self.parent().find('.aset-title').css('visibility', 'hidden');
         //raise parent container annotation-set above code line numbers and code lines table
         //this ensures that code lines with multiple annotation icons are visible
         //without affecting the layout and flow of the adjacent code table
         self.parent().css('z-index', 5);
+        self.parent().addClass('visible');
+        //make the annotation div visible
+        self.next().css('visibility', 'visible');
+        self.next().addClass('visible');
     });
 
     $('.aset-title-close').click(function() {
         //hide a visible annotation message and put parent annotation-set under other elements again
         var self = $(this);
-        self.parent().parent().removeClass('visible');
+        self.parent().removeClass('visible');
+        self.parent().css('visibility', 'hidden');
         self.parent().parent().css('z-index', 0);
+        self.parent().parent().removeClass('visible');
     });
 
 });
